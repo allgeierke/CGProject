d7ffd8d84c565129fd9d0b095b11461e72abb071
Dennis <dennisbroecker@t-online.de>
2022-04-14 19:46:08 +0200

    frische Luft und eine lange Autofahrt bewirkt wahre Wunder :) Task 1.2(Kreis) erledigt

--- src/res/shaders/aufgabe1.glsl
+++ src/res/shaders/aufgabe1.glsl
@@ -3,13 +3,13 @@ out vec3 pixelFarbe; //Name beliebig
 
 bool istImKreis() {
     vec2 m;
-    
-
-    if(distance(m.x,gl_FragCoord.x) < 30 && distance(m.y,gl_FragCoord.y) < 30) {
-        return true;
-    } else {
-        return false;
-    }
+    m.x = 300;
+    m.y = 300;
+        if(distance(m.xy, gl_FragCoord.xy) < 30) {
+            return true;
+        } else {
+            return false;
+        }
 }
 void main() {
     //Hintergrund
@@ -19,10 +19,8 @@ void main() {
     if (gl_FragCoord.x < 200 && gl_FragCoord.y < 200) {
         pixelFarbe = vec3(0.0, 0.5, 0.5);
     }
-
-    //Kreis
-    if(istImKreis) {
-        pixelFarbe = vec3(0.75, 0.5, 0.5);
+    if(istImKreis()) {
+        pixelFarbe = vec3(0.75, 0.0, 0.1);
     }
 
 }
